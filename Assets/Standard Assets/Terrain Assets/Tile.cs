0984bbee3d4ce678d1f7fa128da982a06e2fc50c
Austin Koehler <truyuri@outlook.com>
2015-02-15 23:46:22 -0600

    Cleanups, basic tile work
    -Removed a bunch of Unity skybox materials
    -Created a basic starry skybox
    -Set up materials and prefabs for the hexagon tile.

--- Assets/Standard Assets/Terrain Assets/Tile.cs
+++ Assets/Standard Assets/Terrain Assets/Tile.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 namespace SpaceGame
 {
+    [System.Serializable] 
     public class Tile : MonoBehaviour {
 
 	    // Use this for initialization
 	    void Start () {
-	
 	    }
 	
 	    // Update is called once per frame
