e8f2bd318a7dd649a33ecf9cb0696b8c701f385f
Austin Koehler <truyuri@outlook.com>
2015-02-16 16:02:00 -0600

    New hexagon, changed tiles to sectors.

--- Assets/Standard Assets/Terrain Assets/Sector.cs
+++ Assets/Standard Assets/Terrain Assets/Sector.cs
@@ -1,23 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using System;
 
 namespace SpaceGame
 {
+    [System.Serializable] 
     public class Sector : MonoBehaviour {
-
-        private List<Tile> Tiles;
 
 	    // Use this for initialization
 	    void Start () {
-            Tiles = new List<Tile>();
 	    }
-
-        public void Generate(System.Random seed)
-        {
-
-        }
 	
 	    // Update is called once per frame
 	    void Update () {
