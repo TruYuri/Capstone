e3928b883679ccf4aab24257c0f8dd3fc4988673
Kreestman <KAI93@hotmail.com>
2015-02-25 12:42:54 -0600

    Actual Start Screen
    Accidentally committed wrongly before, this is the preliminary version
    of the screen

---
+++ Assets/StartManagerScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartManagerScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void StartGame()
+	{
+		Application.LoadLevel("Map");
+	}
+}
