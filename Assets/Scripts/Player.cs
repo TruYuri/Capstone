1344a32c88e1454cc61066d28e7f5ac28bcb7ec0
Austin Koehler <truyuri@outlook.com>
2015-04-03 02:13:23 -0500

    Clamp Win Chance

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -125,7 +125,7 @@ public class Player : MonoBehaviour
         else if (pt == null && pt != null)
             WC = _playerSquad.GenerateWinChance(et);
 
-        return WC;
+        return Mathf.Clamp01(WC);
     }
 
     public virtual Squad Battle(float playerChance, Squad player, Squad enemy)
